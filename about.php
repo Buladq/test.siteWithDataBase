d4aa57813fdac2047fd5bf4b211add040f9ddfe7
Bulat <chumakov.bulat@mail.ru>
2022-05-21 01:18:30 +0300

    about me info

---
+++ about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Обо мне</title>
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="fa/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fa/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fa/favicon-16x16.png">
+    <link rel="manifest" href="fa/site.webmanifest">
+    <link rel="mask-icon" href="fa/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="css/mainforaboutcss.css">
+
+
+</head>
+<body>
+
+<div class="page-team">
+    <div class="wrap">
+        <h2 class="title-40 area"><span>Чумаков  </span>Булат</h2>
+
+        <div class="box-team">
+            <div class="box-team-item" >
+                <img class="avatar" src="css/ya3.jpg" alt="Bul">
+                <h2>Студент 2 курса, группы 09-053</h2>
+                <h2>Прикладная информатика</h2>
+                <ul class="social-icons">
+                    <li><a class="social-icon-twitter" href="https://www.instagram.com/buladq/?hl=ru" title="..." target="_blank" rel="noopener"></a></li>
+                    <li><a class="social-icon-vk" href="https://vk.com/niggabezbab" title="..." target="_blank" rel="noopener"></a></li>
+                    <li><a class="social-icon-telegram" href="https://web.telegram.org/z/#984083139" title="..." target="_blank" rel="noopener"></a></li>
+
+                </ul>
+
+
+
+            </div>
+        </div>
+
+
+</body>
+
+</html>
